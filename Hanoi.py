725a9b10a0df343d0d3744a2e1c9c5332b27598f
qunwei <yangqunwei@huawei.com>
2016-01-08 14:21:41 +0800

    recursive algorithm pratice for Hanoi problem.

---
+++ Hanoi.py
@@ -0,0 +1,12 @@
+def move(n, src, dst):
+    print 'move %dth from %s to %s dst' % (n,src,dst)
+
+def hanoi(src, dst, h, n):
+    if n == 0:
+        return
+    hanoi(src, h, dst, n-1)
+    move(n, src, dst)
+    hanoi(h, dst, src, n-1)
+
+if __name__ == "__main__":
+    hanoi("A","C", "B", 4)
